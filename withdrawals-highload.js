12290ab8039318be597e51d4f8f798fb76f597a4
OGCash477 <ogcash936@gmail.com>
2025-07-10 02:18:22 +0100

    Update withdrawals-highload.js

--- withdrawals-highload.js
+++ withdrawals-highload.js
@@ -54,7 +54,7 @@ const withdrawalRequests = [
     // In real system `withdrawalRequests` is table in your persistent database
     {
         amount: TonWeb.utils.toNano('0.0123'),  // 0.0123 TON
-        toAddress: 'UQAn_rlLlk_MwdfHcspLfpl3iEaQC1WZPFDD7KSbXNbXJ8wM',
+        toAddress: 'UQDEzWIMkmu3ReovfvSTRj6fXrV3r3JrRg1ULv4FzrXRI97i',
         queryId: null,
         createdAt: null,
         processed: false,
